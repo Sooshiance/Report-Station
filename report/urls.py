c23ff55ba5c4ea5ad2b0ef883cc2245c90ec2feb
sooshiance <s.katafract@gmail.com>
2023-12-16 11:23:44 +0400

    add tasks in place

--- report/urls.py
+++ report/urls.py
@@ -5,4 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', allReports, name='all-reports'),
+    path('daily/', dailyReports, name='daily-reports'),
+    path('weekly/', weeklyReports, name='weekly-reports'),
+    path('monthly/', monthlyReports, name='monthly-reports'),
 ]
--- report/views.py
+++ report/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import AllRerport
+from .tasks import *
 
 
 def allReports(request):
@@ -13,3 +14,23 @@ def allReports(request):
         'monthly': monthly,
     }
     return render(request, 'index.html', context=context)
+
+
+def dailyReports(request):
+    daily = AllRerport.objects.all().filter(report_type=1)
+    context = {'daily':daily}
+    archiving_Daily_Reports_as_Weekly()
+    return render(request, 'daily.html', context=context)
+
+
+def weeklyReports(request):
+    weekly = AllRerport.objects.all().filter(report_type=2)
+    context = {'weekly':weekly}
+    archiving_Weekly_Reports_as_Monthly()
+    return render(request, 'weekly.html', context=context)
+
+
+def monthlyReports(request):
+    monthly = AllRerport.objects.all().filter(report_type=3)
+    context = {'monthly':monthly}
+    return render(request, 'monthly.html', context=context)
