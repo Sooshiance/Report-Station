c00a9065152e4b724f2d8a698bbc4dda76db2349
sooshiance <s.katafract@gmail.com>
2023-12-17 14:28:48 +0400

    fixing calling tasks

--- report/views.py
+++ report/views.py
@@ -13,20 +13,20 @@ def allReports(request):
         'weekly': weekly,
         'monthly': monthly,
     }
+    archiving_Daily_Reports_as_Weekly()
+    archiving_Weekly_Reports_as_Monthly()
     return render(request, 'index.html', context=context)
 
 
 def dailyReports(request):
     daily = AllRerport.objects.all().filter(report_type=1)
     context = {'daily':daily}
-    archiving_Daily_Reports_as_Weekly()
     return render(request, 'daily.html', context=context)
 
 
 def weeklyReports(request):
     weekly = AllRerport.objects.all().filter(report_type=2)
     context = {'weekly':weekly}
-    archiving_Weekly_Reports_as_Monthly()
     return render(request, 'weekly.html', context=context)
 
 
